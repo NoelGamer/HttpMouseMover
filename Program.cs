58b2087719979233d35ab79605aef86b2fe1489e
NoelGamer <30290751+NoelGamer@users.noreply.github.com>
2024-08-05 12:32:29 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Runtime.InteropServices;
+
+namespace HttpMouseMover
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, uint dwExtraInfo);
+        private const uint MOUSEEVENTF_MOVE = 0x0001;
+
+        static async Task HandleRequest(HttpListenerContext context)
+        {
+            // query format "/move?dx=10&dy=-20"
+            var query = context.Request.QueryString;
+
+            if (int.TryParse(query["dx"], out int dx) && int.TryParse(query["dy"], out int dy))
+            {
+                mouse_event(MOUSEEVENTF_MOVE, dx, dy, 0, 0);
+            }
+
+            context.Response.Close();
+        }
+
+        static async Task StartServer()
+        {
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+            Console.WriteLine("http server started");
+
+            while (true)
+            {
+                var context = await listener.GetContextAsync();
+                _ = Task.Run(() => HandleRequest(context));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            StartServer().GetAwaiter().GetResult();
+        }
+    }
+}
